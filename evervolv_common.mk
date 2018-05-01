4aa6044bbf4cb094a43ff1c63ff628c8c1528529
Jesse Chan <jc@lineageos.org>
2018-04-30 23:02:55 -0700

    overlay: Enable option for full screen aspect ratio
    Change-Id: I2a1b2acc5ed97ccb6795dc371c2b3ac9cb4f9d03

--- evervolv_common.mk
+++ evervolv_common.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/gs101/overlay-ev
+
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
