b5e6e0e16ccafbf96150e6d962019f660d3627c4
razorloves <razorloves@gmail.com>
2018-09-02 23:30:11 -0600

    Set OPA property to enable google assistant
    Change-Id: I741171219d64a28d56531f063f95f3f6f6cf81e6

--- evervolv_common.mk
+++ evervolv_common.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Google Assistant
+PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
+
 # EUICC
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
